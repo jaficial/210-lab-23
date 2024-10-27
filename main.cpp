49542d776233e04a75f5c93dac0e50aac0eba46d
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-26 23:23:38 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -16,6 +16,26 @@ void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
 
+int main_menu(){
+    int choice;
+
+    cout << "*** GOAT MANAGER 3001 ***" << endl;
+    cout << "[1] Add a goat" << endl;
+    cout << "[2] Delete a goat" << endl;
+    cout << "[3] List goats" << endl;
+    cout << "[4] Quit" << endl; 
+    cout << "Choice --> ";
+    cin >> choice;
+
+    if ((choice < 1) || (choice > 4)){
+        cout << "Choice Invalid" << endl;
+    }
+
+    if (choice == 1) {
+
+    }
+}
+
 int main() {
     srand(time(0));
     bool again;
