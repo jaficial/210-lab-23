6fa3350acdccef8a7d9b8fad7c3e81905a1607f0
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-26 23:08:39 -0700

    10-minute update

--- Goat.h
+++ Goat.h
@@ -1,20 +1,30 @@
 // COMSC-210 | Lab 23 | Jed Aficial 
 // github link: https://github.com/jaficial/210-lab-23
 // Goat.h
-//
 #ifndef GOAT_H
 #define GOAT_H
 #include <iostream>
 using namespace std;
 
+/* write three constructors for the Goat class: 
+    - constructor that has name as an argument
+    - constructor with name and age
+    - constructor with all three parameters  
+ */ 
 class Goat {
 private:
     string name;
     int age;
     string color;
 public: 
+    // default constructor (doesn't take any parameters)
     Goat()                          { name = ""; age = 0; color = ""; }
-    // write three more constructors
+    // constructor with name as argument
+    Goat(string goat_name) {name = goat_name;}
+    // constructor with name and age
+    Goat(string goat_name, int goat_age) {name = goat_name; age = goat_age;}
+    // constructor with all parameters
+    Goat(string goat_name, int goat_age, string goat_color) {name = goat_name; age = goat_age; color = goat_color;}
 
     // setters and getters
     void set_name(string n)         { name = n; };
